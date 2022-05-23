5824790e288fab5a1a77a8f66edb471e0d3efd20
Mykolay Chernetskiy <yoko.sh.90@gmail.com>
2022-05-23 20:19:20 +0300

    Go1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -20,19 +20,19 @@ function onFormSubmit(e) {
 }
 
 function onTextareaInput(e) {
-  formData[e.target.name] = e.terget.value;
+  formData[e.target.name] = e.target.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
 function populareTextarea() {
-  const savedMessage = localStorage.getItem('feedback-form-state');
-  if (savedMessage) {
-    const savedData = JSON.parse(savedMessage);
-    if (savedData.email) {
-      emailEll.value = savedData.email;
+  const saveMessage = localStorage.getItem('feedback-form-state');
+  if (saveMessage) {
+    const saveData = JSON.parse(saveMessage);
+    if (saveData.email) {
+      emailEll.value = saveData.email;
     }
-    if (savedData.message) {
-      messageEll.value = savedData.message;
+    if (saveData.message) {
+      messageEll.value = saveData.message;
     }
   }
 }
