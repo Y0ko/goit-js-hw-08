62746d56391680d01613a4bfd0ea325aa51ccb64
Mykolay Chernetskiy <yoko.sh.90@gmail.com>
2022-05-27 00:42:46 +0300

    return 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,38 +1,39 @@
 const throttle = require('lodash.throttle');
 
 const formEll = document.querySelector('.feedback-form');
-const emailEll = formEll.querySelector('input');
-const messageEll = formEll.querySelector('textarea');
+
 let formData = {};
 
-formEll.addEventListener('input', throttle(onTextareaInput, 500));
+const LOCALSTORAGE_K = 'feedback-form-state';
+
+formEll.addEventListener('input', throttle(onFormInput, 500));
 formEll.addEventListener('submit', onFormSubmit);
 
 populareTextarea();
 
-function onFormSubmit(e) {
-  e.preventDefault();
-  e.currentTarget.reset();
-  const saveData = localStorage.getItem('feedback-form-state');
-  console.log(JSON.parse(saveData));
-  localStorage.removeItem('feedback-form-state');
-  formData = {};
+function onFormInput() {
+  localStorage.setItem(LOCALSTORAGE_K, JSON.stringify(statusObject()));
 }
 
-function onTextareaInput(e) {
-  formData[e.target.name] = e.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(statusObject());
+  formEll.reset();
+  localStorage.removeItem(LOCALSTORAGE_K);
+}
+
+function statusObject() {
+  return {
+    email: formEll.elements.email.value,
+    message: formEll.elements.message.value,
+  };
 }
 
 function populareTextarea() {
-  const saveMessage = localStorage.getItem('feedback-form-state');
-  if (saveMessage) {
-    const saveData = JSON.parse(saveMessage);
-    if (saveData.email) {
-      emailEll.value = saveData.email;
-    }
-    if (saveData.message) {
-      messageEll.value = saveData.message;
-    }
-  }
+  let currentFieldStatus;
+  try {
+    currentFieldStatus = JSON.parse(localStorage.getItem(LOCALSTORAGE_K));
+    formEll.elements.email.value = currentFieldStatus.email;
+    formEll.elements.message.value = currentFieldStatus.message;
+  } catch (error) {}
 }
